a93d604e7708135421f8f0d3ca2a87204570b6a3
MANU MENDOZA <manumendoza@MacBook-Pro-de-MANU.local>
2025-01-03 23:30:38 -0400

    fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const ffmpeg = require("fluent-ffmpeg");
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const broadcastManager = require('./broadcast');
